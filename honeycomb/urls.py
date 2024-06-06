7be14e5ed08ac1ac147ddebbfb56becabdc53592
Sina <sadeghesfahani.sina@gmail.com>
2024-06-06 20:22:20 +0200

    Add separate membership submit and accept serializers and views
    Two new serializer classes for hive membership has been added - MembershipAcceptSerializer and MembershipSubmitSerializer. Corresponding views, MembershipSubmitView and MembershipAcceptView, have also been added. The 'Membership' view has been refactored to 'MembershipAcceptView' and includes the new serializer. Additional changes to this view include enhanced error messages and conditions check for the hive request.

--- honeycomb/urls.py
+++ honeycomb/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import MembershipSubmitView, MembershipAcceptView
+from .views import Membership
 
 router = DefaultRouter()
 router.register(r'hives', views.HiveViewSet)
