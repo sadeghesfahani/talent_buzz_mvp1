0267ef1a6f82c419cf5a8ac50ceb8647cf1a050f
Sina <sadeghesfahani.sina@gmail.com>
2024-06-06 11:07:08 +0200

    Add UserIDView to authentication views and URLs
    user id can optain with a get request to this endpoint with given credentials
    
    The new UserIDView has been added to the authentication views. It makes use of the Django Rest Framework's APIView, permission classes, and response. Furthermore, a new URL pattern for the UserIDView has been added to the urlpatterns in the authentication's URLs file.

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from authentication.views import UserIDView
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user_info/', UserIDView.as_view(), name='user_id'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class UserIDView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"user_id": request.user.id})
