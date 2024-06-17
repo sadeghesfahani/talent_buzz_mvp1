935b158d03f00fa02d93048ff94d1f0b06743530
Sina <sadeghesfahani.sina@gmail.com>
2024-06-17 12:10:58 +0200

    Add functionality to handle required actions in AI response
    The AI service has been enhanced to process "requires_action" status in AI responses. This includes parsing function arguments and executing the referenced function. Additionally, the BeeService has been introduced for fetching and manipulating "Bee" entities and BeeSerializer has been employed to handle serialization. One of such functions, 'show_bees_to_user', is added as an example.

--- ai/services.py
+++ ai/services.py
@@ -1,9 +1,11 @@
+import json
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from openai import OpenAI
-from openai.types.beta.threads import Run
 
-from honeycomb.honeycomb_service import HiveService
+from honeycomb.honeycomb_service import HiveService, BeeService
+from honeycomb.serializers import BeeSerializer
 from .helpers import AIBaseClass
 from .models import Message
 
@@ -17,6 +19,7 @@ class AIService:
         self.user = user
         self.ai = AIBaseClass(assistant_type)
         self.hive_service = HiveService()
+        self.bee_service = BeeService()
         self.thread_id = self.ai.get_or_create_thread_by_user(user).id
 
     def send_message(self, message: str, additional_instructions: str = "",
@@ -40,8 +43,80 @@ class AIService:
         run = self.ai.run(self.ai.get_assistant(ai_type).id, thread.id, additional_instructions)
         return self.process_ai_response(run)
 
-    def process_ai_response(self, run: Run):
-        if run.status == "completed":
+    def process_ai_response(self, run):
+        if run.status == "requires_action":
+            function_responses = list()
+            for tool in run.required_action.submit_tool_outputs.tool_calls:
+                arguments = tool.function.arguments
+                # parse string to dict
+                try:
+                    arguments = json.loads(arguments)
+                except json.JSONDecodeError as e:
+                    print(f"Error parsing arguments: {e}")
+                    arguments = {}
+                _function = self.get_function_reference(tool.function.name)
+                function_response = _function(**arguments)
+                if isinstance(function_response, str):
+                    function_responses.append({
+                        "tool_call_id": tool.id,
+                        "output": function_response
+                    })
+                else:
+                    function_responses.append({
+                        "tool_call_id": tool.id,
+                        "output": "Error processing function"
+                    })
+
+            return self.process_ai_response(
+                client.beta.threads.runs.submit_tool_outputs_and_poll(tool_outputs=function_responses, run_id=run.id,
+                                                                      thread_id=run.thread_id))
+
+        elif run.status == "completed":
             response = client.beta.threads.messages.list(thread_id=run.thread_id)[-1]
             self.last_message.response = response.content
+            self.last_message.save()
             return response.content
+
+    def show_bees_to_user(self, bees_id_list: [str]) -> str:
+        bees_queryset = self.bee_service.get_bee_queryset(bees_id_list)
+        bees = BeeSerializer(bees_queryset, many=True)
+        print(bees.data)
+        return "data has been shown successfully"
+
+    def get_function_reference(self, function_name: str):
+        functions = {
+            "show_bees_to_user": self.show_bees_to_user,
+        }
+        return functions.get(function_name, lambda **kwargs: print("Function not found"))
+
+### example of response of run
+#
+# {
+#   "id": "run_qJL1kI9xxWlfE0z1yfL0fGg9",
+#   ...
+#   "status": "requires_action",
+#   "required_action": {
+#     "submit_tool_outputs": {
+#       "tool_calls": [
+#         {
+#           "id": "call_FthC9qRpsL5kBpwwyw6c7j4k",
+#           "function": {
+#             "arguments": "{"location": "San Francisco, CA"}",
+#             "name": "get_rain_probability"
+#           },
+#           "type": "function"
+#         },
+#         {
+#           "id": "call_RpEDoB8O0FTL9JoKTuCVFOyR",
+#           "function": {
+#             "arguments": "{"location": "San Francisco, CA", "unit": "Fahrenheit"}",
+#             "name": "get_current_temperature"
+#           },
+#           "type": "function"
+#         }
+#       ]
+#     },
+#     ...
+#     "type": "submit_tool_outputs"
+#   }
+# }
--- honeycomb/honeycomb_service.py
+++ honeycomb/honeycomb_service.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+from honeycomb.models import Bee, Contract, Nectar
 
 
 class HiveService:
@@ -47,3 +48,34 @@ class NectarService:
     @staticmethod
     def accept_nectar_application(application, user):
         application.accept_application(user)
+
+
+class BeeService:
+    @staticmethod
+    def get_bee(bee_id) -> 'Bee':
+        from .models import Bee
+        return Bee.objects.get(id=bee_id)
+
+    def get_bee_queryset(self, list_of_bee_ids:[str]):
+        from .models import Bee
+        return Bee.objects.filter(id__in=list_of_bee_ids)
+
+    @staticmethod
+    def get_user_bees(user: User) -> ['Bee']:
+        from .models import Bee
+        return Bee.objects.filter(user=user)
+
+    @staticmethod
+    def get_hive_bees(hive) -> ['Bee']:
+        from .models import Bee
+        return Bee.objects.filter(hive=hive)
+
+    @staticmethod
+    def get_bee_contracts(bee) -> ['Contract']:
+        from .models import Contract
+        return Contract.objects.filter(bee=bee)
+
+    @staticmethod
+    def get_bee_nectars(bee) -> ['Nectar']:
+        from .models import Nectar
+        return Nectar.objects.filter(bee=bee)
