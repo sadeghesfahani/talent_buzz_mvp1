7b15f5141626c636b65475c9c27616a8bb2deb9c
Sina <sadeghesfahani.sina@gmail.com>
2024-07-06 04:55:48 +0200

    Add hashing and document re-use to Document model
    The commit introduces hashing for document files and checks for the existence of the same hash before saving a new document. If the same hash exists, the current instance of document will reuse the existing document's details. The save method in Document model has been updated accordingly and a new method to calculate the hash has been added.

--- common/models.py
+++ common/models.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
@@ -14,25 +16,54 @@ class Document(models.Model):
     file_id = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
     isolated_vector_store = models.CharField(max_length=255, blank=True)
+    hash = models.CharField(max_length=255, unique=True, blank=True)
+    is_ai_sync = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.personal_details.first_name + " " + self.user.personal_details.last_name + " - " + self.document.name
 
-    def save(
-            self, force_insert=False, force_update=False, using=None, update_fields=None
-    ):
-        if not self.file_id:
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+
+        if self.document:  # Check if there is a document to process
+            file_hash = self.calculate_hash(self.document)  # Calculate hash
+            existing_document = Document.objects.filter(hash=file_hash).first()
+            if existing_document:
+                # If an existing document is found, point the current instance to it
+                self.id = existing_document.id  # Set the current object's ID to the existing one
+                self.document = existing_document.document  # Re-use the existing document file
+                self.file_id = existing_document.file_id  # Re-use the existing file_id
+                if update_fields:
+                    # Only update fields that are explicitly listed
+                    super().save(force_insert, force_update, using, update_fields)
+                return
+            else:
+                self.hash = file_hash  # Set the new hash if no document is found
+
+        if self.is_ai_sync and not self.file_id:
+            # Upload file and set file_id only if it's a new file and no existing document was found
             uploaded_file = client.files.create(
                 file=(self.document.file.name, self.document.file.open('rb').read()), purpose="assistants"
             )
             self.file_id = uploaded_file.id
+
+            # Proceed with saving as a new document
         super().save(force_insert, force_update, using, update_fields)
 
     def convert_to_ai_readable(self):
         return f"""
         representetive file_id in vectore store is : {self.file_id}, user document description is : {self.description}
         """
+
+    @staticmethod
+    def calculate_hash(file_field):
+        """
+        Calculate MD5 hash of a file.
+        """
+        md5_hash = hashlib.md5()
+        for chunk in file_field.chunks():
+            md5_hash.update(chunk)
+        return md5_hash.hexdigest()
 
 
 @receiver(post_save, sender=Document)
